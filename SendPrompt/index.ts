0a338db9e2bc0695a5a6104887993e736da471fd
lucasamonrc <lucasamonrc@gmail.com>
2022-08-09 11:01:51 -0600

    Implement send messages endpoint

--- SendPrompt/index.ts
+++ SendPrompt/index.ts
@@ -1,17 +1,23 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import 'dotenv/config';
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const message = await client.messages.create({
+    body: 'What are three things to which you are grateful for today?',
+    from: '+12565593246',
+    to: '+13852950754',
+  });
 
+  context.res = {
+    body: `Message ${message.sid} was sent.`,
+  };
 };
 
 export default httpTrigger;

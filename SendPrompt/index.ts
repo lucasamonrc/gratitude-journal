c2240d0db88b074567c77331eba155d9a3ab9665
lucasamonrc <lucasamonrc@gmail.com>
2022-08-09 21:21:04 -0600

    Change SendPrompt function to be timerTrigger

--- SendPrompt/index.ts
+++ SendPrompt/index.ts
@@ -1,23 +1,25 @@
 import 'dotenv/config';
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { AzureFunction, Context } from '@azure/functions';
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const httpTrigger: AzureFunction = async function (
+const timerTrigger: AzureFunction = async function (
   context: Context,
-  req: HttpRequest
+  myTimer: any
 ): Promise<void> {
+  if (myTimer.isPastDue) {
+    context.log('Timer function is running late!');
+  }
+
   const message = await client.messages.create({
     body: 'What are three things to which you are grateful for today?',
     from: '+12565593246',
     to: '+13852950754',
   });
 
-  context.res = {
-    body: `Message ${message.sid} was sent.`,
-  };
+  context.log(`Message ${message.sid} was sent.`);
 };
 
-export default httpTrigger;
+export default timerTrigger;
